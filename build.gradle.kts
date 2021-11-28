5bfb597db9a859cad7fa430456274f24d7e28bb1
Oleksandr Kolodkin <alexandr.kolodkin@gmail.com>
2021-11-28 22:57:02 +0200

    Initial release

---
+++ build.gradle.kts
@@ -0,0 +1,72 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.text.SimpleDateFormat
+import java.util.Date
+
+plugins {
+    application
+    kotlin(Kotlin.jvmId) version Kotlin.version
+    kotlin(Kotlin.kaptId) version Kotlin.version
+    shadow(Shadow.id) version Shadow.version
+}
+
+project.ext {
+    set("mainClassName", "io.github.kolod.GroundTruthEditor")
+}
+
+group = "io.github.kolod"
+version = SimpleDateFormat("yy.M.d").format(Date())
+
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation(Kotlin.stdlibJdk8)
+    implementation(Logger.core)
+    implementation(Logger.api)
+    implementation(Logger.slf4j)
+    implementation(Manifests.core)
+    implementation(FlatLookAndFeel.core)
+    implementation(FlatLookAndFeel.intellij)
+    implementation(FlatLookAndFeel.extras)
+    testImplementation(kotlin("test"))
+    testImplementation(platform(Junit.platform))
+    testImplementation(Junit.core)
+}
+
+application {
+    mainClass.set(project.ext["mainClassName"] as String)
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = Jvm.version
+    }
+}
+
+tasks.withType<ShadowJar> {
+    manifest {
+        attributes["Implementation-Title"] = "GroundTruthEditor"
+        attributes["Main-Class"] = project.ext["mainClassName"] as String
+        attributes["Author"] = "Oleksandr Kolodkin"
+        attributes["Created-By"] = "Gradle ${gradle.gradleVersion}"
+        attributes["Multi-Release"] = "true"
+        attributes["Build-Date"] = SimpleDateFormat("dd/MM/yyyy").format(Date())
+    }
+}
+
+tasks {
+    build {
+        dependsOn(shadowJar)
+    }
+}
---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,75 @@
+@file:Suppress("MemberVisibilityCanBePrivate", "unused")
+
+import org.gradle.plugin.use.PluginDependenciesSpec
+import org.gradle.plugin.use.PluginDependencySpec
+
+fun PluginDependenciesSpec.shadow(module: String): PluginDependencySpec = id(module)
+
+object Shadow {
+	const val version = "7.1.0"
+	const val id = "com.github.johnrengelman.shadow"
+}
+object Jvm {
+	const val version = "1.8"
+}
+
+object Kotlin {
+	const val version = "1.6.0"
+	const val stdlibJdk8 = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
+	const val jvmId = "jvm"
+	const val kaptId = "kapt"
+}
+
+object JNA {
+	const val version = "5.9.0"
+	const val core = "net.java.dev.jna:jna:$version"
+	const val platform = "net.java.dev.jna:jna-platform:$version"
+}
+
+object Logger {
+	const val version = "2.14.1"
+	const val core = "org.apache.logging.log4j:log4j-core:$version"
+	const val api = "org.apache.logging.log4j:log4j-api:$version"
+	const val slf4j = "org.apache.logging.log4j:log4j-slf4j-impl:$version"
+}
+
+object Manifests {
+	const val version = "1.1"
+	const val core = "com.jcabi:jcabi-manifests:$version"
+}
+
+object FlatLookAndFeel {
+	const val version = "1.6.4"
+	const val core = "com.formdev:flatlaf:$version"
+	const val intellij = "com.formdev:flatlaf-intellij-themes:$version"
+	const val extras = "com.formdev:flatlaf-extras:$version"
+}
+
+object SqLite {
+	const val version = "3.36.0.3"
+	const val core = "org.xerial:sqlite-jdbc:$version"
+}
+
+object Exposed {
+	const val version = "0.36.2"
+	const val core = "org.jetbrains.exposed:exposed-core:$version"
+	const val dao = "org.jetbrains.exposed:exposed-dao:$version"
+	const val jdbc = "org.jetbrains.exposed:exposed-jdbc:$version"
+	const val time = "org.jetbrains.exposed:exposed-java-time:$version"
+}
+
+object Tesseract {
+	const val version = "4.5.5"
+	const val core = "net.sourceforge.tess4j:tess4j:$version"
+}
+
+object Junit {
+	const val version = "5.8.1"
+	const val core = "org.junit.jupiter:junit-jupiter:$version"
+	const val platform = "org.junit:junit-bom:$version"
+}
+
+object Kolod {
+	const val group = "io.github.kolod"
+	const val appender = "$group:log4j-textarea-appender:1.0"
+}
